a82aeeafec42d06c66e41c6853a90c57425eab15
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-05-24 11:09:31 +0700

    add imafe

--- src/pages/hostCreateHotel/components/ImageSelect/ImageSelect.jsx
+++ src/pages/hostCreateHotel/components/ImageSelect/ImageSelect.jsx
@@ -3,17 +3,18 @@ import './ImageSelect.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faPencil } from '@fortawesome/free-solid-svg-icons';
 
-const ImageSelect = ({ handleClear, handleChange, imgUrl, border = false }) => {
+const ImageSelect = ({ handleClear, handleChange, imgUrl, border = false, name }) => {
+	console.log(name, imgUrl)
 	return (
-		<div className={ border ? 'selectImgWapper borderCustom' : "selectImgWapper" } style={ { backgroundImage: `url(${imgUrl})` } }>
+		<div className={ border ? 'selectImgWapper borderCustom' : "selectImgWapper" } style={ { backgroundImage: `url(${imgUrl?.preview})` } }>
 			{
-				imgUrl ? <button onClick={ handleClear } className="editBtn editClear"><FontAwesomeIcon icon={ faXmark } /></button>
+				imgUrl?.preview ? <button onClick={ handleClear } className="editBtn editClear"><FontAwesomeIcon icon={ faXmark } /></button>
 					:
-					<label className="editBtn" htmlFor="img1">
+					<label className="editBtn" htmlFor={ name }>
 						<FontAwesomeIcon icon={ faPencil } className="iconEdit" />
 					</label>
 			}
-			<input type="file" className="inputEdit" id="img1" name="img1" onChange={ handleChange } />
+			<input type="file" className="inputEdit" id={ name } name={ name } onChange={ handleChange } />
 		</div>
 	)
 }
