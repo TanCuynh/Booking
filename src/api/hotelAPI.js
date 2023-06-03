f3d0fd61bae03580bf84787404d88d24dcd1436e
Tấn Cuỳnh <binphan120302@gmail.com>
2023-06-03 09:50:53 +0700

    add API getHotelByCity

--- src/api/hotelAPI.js
+++ src/api/hotelAPI.js
@@ -3,8 +3,12 @@ import axiosClient from "./axiosClient";
 const hotelAPI = {
 	getAllHotel:() => axiosClient.get(`/hotels`),
 	getHotelById: (id) => axiosClient.get(`/hotels/${id}`),
-	getHotelByCity: (city) => axiosClient.get(`/hotels/${city}`),
-	getCategoryByHotelId: (id) => axiosClient.get(`/categories/hotels/${id}`),
+	getHotelByCity: (city) => axiosClient.get(`/hotels/city/${city}`),
+	getHotelByName: (name) => axiosClient.get(`/hotels/name/${name}`),
+	getHotelByAddress: (address) => axiosClient.get(`/hotels/address/${address}`),
+	getHotelByNation: (nation) => axiosClient.get(`/hotels/nation/${nation}`),
+	getHotelByPrice: (min_price, max_price) => axiosClient.get(`/hotels/price?min_price=${min_price}&max_price=${max_price}`),
+	getHotelByRating: (rating) => axiosClient.get(`/hotels/rating?rating=${rating}`),
 }
 
 export default hotelAPI;
