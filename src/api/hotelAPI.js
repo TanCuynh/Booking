0888914fb89245544c4eadd331f1f0f7d9c5db2f
Tấn Cuỳnh <binphan120302@gmail.com>
2023-06-05 23:36:18 +0700

    add category API, fix featuredProperty images

--- src/api/hotelAPI.js
+++ src/api/hotelAPI.js
@@ -5,11 +5,14 @@ const hotelAPI = {
 	getHotelById: (id) => axiosClient.get(`/hotels/${id}`),
 	getLatestHotel: () => axiosClient.get(`/hotels/latest`),
 	getHotelNearby: (city) => axiosClient.get(`/hotels/nearby`),
+
+	getMostBookedHotel: () => axiosClient.get(`/hotels/top-booked`),
 	getHotelByName: (name) => axiosClient.get(`/hotels/name/${name}`),
 	getHotelByAddress: (address) => axiosClient.get(`/hotels/address/${address}`),
 	getHotelByNation: (nation) => axiosClient.get(`/hotels/nation/${nation}`),
 	getHotelByPrice: (min_price, max_price) => axiosClient.get(`/hotels/price?min_price=${min_price}&max_price=${max_price}`),
 	getHotelByRating: (rating) => axiosClient.get(`/hotels/rating?rating=${rating}`),
+
 
 	getImageByHotel: (id) => axiosClient.get(`/hotel-images/hotel/${id}`),
 	
