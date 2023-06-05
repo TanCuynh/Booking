ef858a8a921ae7c5ab45193a7a414af038f94a5c
Dangngockhanh <dangngockhanhv@gmail.com>
2023-06-05 22:37:24 +0700

    fix thay đổi thông tin cá nhân

---
+++ src/components/changePassword/ChangePassword.jsx
@@ -0,0 +1,17 @@
+import React from "react";  
+import './changePassword.css';
+
+const ChangePassword = () => {
+    return (
+        <div>
+            <p style={{
+                    fontSize: '30px',
+                    fontWeight: 'bold',
+                    marginLeft: '15px',
+            }}
+            >Đổi Mật Khẩu</p>
+        </div>
+    );
+};
+
+export default ChangePassword;
