18e01b2f87be8958d37bd8b1d06f7ed17cb95957
Dangngockhanh <dangngockhanhv@gmail.com>
2023-06-06 17:53:07 +0700

    XongFE danh sách đặt phòng

---
+++ src/components/reservationscomponents/upcoming/Upcoming.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BookingOder from "../../bookingoder/BookingOder";
+import './upcoming.css'
+
+const Upcoming = () => {
+    return (
+        <div>
+            <BookingOder/>
+            <BookingOder/>
+            <BookingOder/>
+        </div>
+    );
+}
+
+export default Upcoming;
