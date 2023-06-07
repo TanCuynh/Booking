1b4ec53d170652b6fa98a1fe539cfcac84fbeeb5
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-06-07 21:49:39 +0700

    add api /me get infor by token

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -3,4 +3,5 @@ import axiosClient from "./axiosClient";
 export const AuthAPI = {
   login: (params) => axiosClient.post("/login", params),
   signup: (params) => axiosClient.post("/register", params),
+  getUserByToken: () => axiosClient.post("/me")
 };
